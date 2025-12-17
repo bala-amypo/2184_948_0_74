06162f4c165cbdb3959bea2753f257e7a208b589
bala-amypo <bala@amypo.in>
2025-12-17 09:56:40 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/studentServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/studentServiceImpl.java
@@ -1,7 +1,12 @@
-package com.example.demo.service.service.impl;
+package com.example.demo.service.impl;
 
 import com.example.demo.entity.*;
 import com.example.demo.service.*;
 
+@Service
+
+public class studentService{
+    
+}
 
 
